97e18f4f7214e05636d9bcc538bc2ad49186c3b6
Michel Can <mchl.aauee.98@gmail.com>
2017-02-09 12:02:01 -0500

    resolved typo in install file

--- install.bash
+++ install.bash
@@ -2,7 +2,7 @@
 sudo apt-get install build-essential libpcre3 libpcre3-dev libssl-dev
 wget http://nginx.org/download/nginx-1.9.15.tar.gz
 wget https://github.com/arut/nginx-rtmp-module/archive/master.zip
-tar -zxvf ngnx-1.9.15.tar.gz
+tar -zxvf nginx-1.9.15.tar.gz
 unzip master.zip
 cd nginx-1.9.15
 ./configure --with-http_ssl_module --add-module=../nginx-rtmp-module-master
